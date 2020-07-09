6e93526d35c38a136bffec2ecb85e7db5f557f4a
codewithirene567 <irenejinscott@gmail.com>
2020-07-08 20:25:21 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#get 'posts/:id', to: 'posts#show' #this is saying make a route called this go to the posts controller with the show action
+resources :posts, only: :show #new refactored route
 end
